ba37756fb80e76dc955f57e18492c63c29316bba
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2024-07-10 10:38:31 +0100

    ELM-2308: Create a python module that checks for changes to propagated routes (#3)

--- README.md
+++ README.md
@@ -14,7 +14,10 @@ module "tgw_route_propagation_monitor" {
 <!-- BEGIN_TF_DOCS -->
 ## Requirements
 
-No requirements.
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.0.1 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | ~> 5.0 |
 
 ## Providers
 
--- function/src/lambda_handler.py
+++ function/src/lambda_handler.py
@@ -1,13 +1,59 @@
+import os
+from typing import Any
+
+import boto3
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger = Logger()
 
-from typing import Any
+
+def construct_message(route_table_id: str) -> str:
+    return f"Routes for transit gateway ({route_table_id}) have changed."
 
 
-@logger.inject_lambda_context
+def get_aws_account_id() -> str:
+    sts_client = boto3.client("sts")
+    caller_identity = sts_client.get_caller_identity()
+
+    return caller_identity["Account"]
+
+
+@logger.inject_lambda_context(log_event=True)
 def handle_event(event: dict, context: LambdaContext) -> dict[Any, Any]:
-    logger.info("Hello world")
+    route_table_id = event["transit-gateway-route-table-id"]
+    known_routes = event["known-routes"]
+
+    logger.debug(f"Route table id: {route_table_id}")
+    logger.debug(f"Expected routes: {known_routes}")
+
+    ec2_client = boto3.client("ec2", region_name="eu-west-2")
+    actual_routes = ec2_client.search_transit_gateway_routes(
+        TransitGatewayRouteTableId=route_table_id,
+        Filters=[{"Name": "type", "Values": ["propagated"]}],
+    )["Routes"]
+
+    logger.info(f"Found {len(actual_routes)} route(s) for route table {route_table_id}")
+
+    unknown_routes = [route for route in actual_routes if route not in known_routes]
+
+    logger.info(
+        f"Found {len(unknown_routes)} unknown route(s) for route table {route_table_id}"
+    )
+
+    missing_routes = [route for route in known_routes if route not in actual_routes]
+
+    logger.info(
+        f"Found {len(missing_routes)} missing route(s) for route table {route_table_id}"
+    )
+
+    sns_client = boto3.client("sns", region_name="eu-west-2")
+
+    if len(missing_routes) > 0 or len(unknown_routes) > 0:
+        sns_client.publish(
+            TopicArn=os.getenv("SNS_TOPIC_ARN"),
+            Subject=f"{get_aws_account_id()} - Transit Gateway Route Table Monitor",
+            Message=construct_message(route_table_id),
+        )
 
     return event
